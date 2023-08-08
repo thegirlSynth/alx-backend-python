e2eb5931709bdd3f68d9efbce10611df23b1a4ef
Cynthia Uche <61497064+thegirlSynth@users.noreply.github.com>
2023-08-08 14:24:10 +0000

    Update return type for 1-concurrent_coroutines.py

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -7,7 +7,7 @@ import random
 wait_random = __import__('0-basic_async_syntax').wait_random
 
 
-async def wait_n(n: int, max_delay: int) -> float:
+async def wait_n(n: int, max_delay: int) -> List[float]:
     """
     This coroutine spawns wait_random a couple of times,
     and then returns a list of delays
