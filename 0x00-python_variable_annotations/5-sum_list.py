cb73f4cb980af08209fea50637b253586fab1d7a
The Cynthia Uche <cynthianuchepro@gmail.com>
2023-08-09 17:36:25 +0100

    Create 5-sum_list.py

---
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""Basic annotations - sum_list
+"""
+
+
+def sum_list(input_list: float) -> float:
+    """Type-annotated function sum_list which takes a list input_list of floats
+    as argument and returns their sum as a float.
+    """
+    return sum(input_list)
