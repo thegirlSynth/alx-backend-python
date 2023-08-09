58dd71aa907d7a5e7dad9db6bc3793a3fda33065
Cynthia Uche <61497064+thegirlSynth@users.noreply.github.com>
2023-08-09 02:11:44 +0000

    Create an async generator

---
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+"""
+Contains a coroutine async_generator"
+"""
+
+import asyncio
+import random
+
+
+async def async_generator() -> float:
+    """
+    Generates 10 random floats between 0 and 10
+    """
+
+    for _ in range(10):
+        await asyncio.sleep(1)
+        yield random.uniform(0, 10)
