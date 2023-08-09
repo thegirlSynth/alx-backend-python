2b3ebb39628bf014543f686e570ab8d4710d735e
The Cynthia Uche <cynthianuchepro@gmail.com>
2023-08-09 16:58:50 +0100

    Create 1-concat.py

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+"""Basic annotations"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """Concatenate two strings and return the result as a string"""
+    return str1 + str2
