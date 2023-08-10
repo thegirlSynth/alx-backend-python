862f6e62eaaa102add205ceee4325d2536eb7307
The Cynthia Uche <cynthianuchepro@gmail.com>
2023-08-10 01:00:52 +0100

    Create a complex type annotation

---
+++ 0x00-python_variable_annotations/7-to_kv.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""
+Complex types - string and int/float to tuple
+"""
+
+from typing import Union, Tuple
+
+
+def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
+    """
+    Takes a string k and an int OR float v as arguments and returns a tuple.
+    """
+    return (k, v * v)
