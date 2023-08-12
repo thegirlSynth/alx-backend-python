a57687fee6a5371c01a1eb08ad17c53dc2afbb61
Cynthia Uche <cynthianuchepro@gmail.com>
2023-08-12 01:35:01 +0100

    Correctly annotate function return type

--- 0x02-python_async_comprehension/1-async_comprehension.py
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -3,12 +3,12 @@
 Async Comprehensions
 """
 
-from typing import Generator
+from typing import List
 
 async_generator = __import__("0-async_generator").async_generator
 
 
-async def async_comprehension():
+async def async_comprehension() -> List[float]:
     """
     Collects 10 random numbers using an async comprehensing over async_generator,
     then returns the 10 random numbers.
