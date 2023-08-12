e7dee917dbba064c606248b2faafd06800d9598f
Cynthia Uche <cynthianuchepro@gmail.com>
2023-08-12 01:38:52 +0100

    Format to PEP coding style

--- 0x02-python_async_comprehension/1-async_comprehension.py
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -10,8 +10,8 @@ async_generator = __import__("0-async_generator").async_generator
 
 async def async_comprehension() -> List[float]:
     """
-    Collects 10 random numbers using an async comprehensing over async_generator,
-    then returns the 10 random numbers.
+    Collects 10 random numbers using an async comprehensing over
+    async_generator, then returns the 10 random numbers.
     """
     num_list = [num async for num in async_generator()]
     return num_list
