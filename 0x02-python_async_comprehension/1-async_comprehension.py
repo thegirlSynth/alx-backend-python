64acb30fbe22e62ef1765d409a5cdf9c1698e00e
Cynthia Uche <cynthianuchepro@gmail.com>
2023-08-12 01:28:26 +0100

    Comprehense over an async generator

---
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+"""
+Async Comprehensions
+"""
+
+from typing import Generator
+
+async_generator = __import__("0-async_generator").async_generator
+
+
+async def async_comprehension():
+    """
+    Collects 10 random numbers using an async comprehensing over async_generator,
+    then returns the 10 random numbers.
+    """
+    num_list = [num async for num in async_generator()]
+    return num_list
