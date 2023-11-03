342e377e873cadf20b1da0975b0879d30a9fdd60
Cynthia Uche - [Linux] <cynthianuchepro@gmail.com>
2023-11-03 22:14:31 +0100

    Test and mock decorator function

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -1,15 +1,12 @@
 #!/usr/bin/env python3
 """
-Parameterize a unit test
+Tests methods of the utils module
 """
 
 from parameterized import parameterized
 import unittest
 from unittest.mock import Mock, patch
-from utils import (
-    get_json,
-    access_nested_map,
-)
+from utils import get_json, access_nested_map, memoize
 
 
 class TestAccessNestedMap(unittest.TestCase):
@@ -54,6 +51,32 @@ class TestGetJson(unittest.TestCase):
             result = get_json(test_url)
             self.assertEqual(result, test_payload)
             mock_requests_get.assert_called_once_with(test_url)
+
+
+class TestMemoize(unittest.TestCase):
+    """
+    Tests that the utils.memoize decorator functions properly.
+    """
+
+    def test_memoize(self):
+        class TestClass:
+            def a_method(self):
+                return 42
+
+            @memoize
+            def a_property(self):
+                return self.a_method()
+
+        with patch.object(TestClass, "a_method", return_value=42) as mocked:
+            test_class = TestClass()
+
+            result1 = test_class.a_property
+            result2 = test_class.a_property
+
+            self.assertEqual(result1, 42)
+            self.assertEqual(result2, 42)
+
+            mocked.assert_called_once()
 
 
 if __name__ == "__main__":
